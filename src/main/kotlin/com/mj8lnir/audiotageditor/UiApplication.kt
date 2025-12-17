676511dc398ae9a0da22e286c7bbb50c973d3d52
Mj8lnir <yabrovn@gmail.com>
2025-12-17 22:36:03 +0500

    deploy-win: small fixes

--- src/main/kotlin/com/mj8lnir/audiotageditor/UiApplication.kt
+++ src/main/kotlin/com/mj8lnir/audiotageditor/UiApplication.kt
@@ -9,11 +9,8 @@ import org.springframework.context.ConfigurableApplicationContext
 
 internal class UiApplication : Application() {
 
-    private lateinit var context: ConfigurableApplicationContext
-
-    override fun init() {
-        context = SpringApplicationBuilder(AudioTagEditorApplication::class.java).run()
-    }
+    private val context: ConfigurableApplicationContext =
+        SpringApplicationBuilder(AudioTagEditorApplication::class.java).run()
 
     override fun start(stage: Stage) {
         context.publishEvent(StageReadyEvent(stage))
